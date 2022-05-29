5c3e32ef3ea192f8a47d0a3f2fac0980b5a8564b
BekhterevaElena <bekhterevaelena92@gmail.com>
2022-05-29 20:04:58 +0400

    added interruption when passing an obstacle unsuccessfully

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import ru.gb.java2.lesson1.*;
 
 public class Main {
     public static void main(String[] args) {
-        Action[] members = {new Human(3,3), new Robot(6,8), new Cat(1,2)};
+        Action[] members = {new Human(3,3), new Robot(10,10), new Cat(1,2)};
         Barrier[] barriers = {new Wall(5), new Treadmill(6), new Wall(8), new Treadmill(1)};
 
         for (int i=0; i < members.length; i++) {
@@ -11,6 +11,7 @@ public class Main {
                     members[i].jump();
                     if (barriers[g].getHeight() >= members[i].getMaxHeight()) {
                         System.out.println("не прыгнул");
+                        break;
                     }
                     else System.out.println("уии пригнул");
 
@@ -19,6 +20,7 @@ public class Main {
                     members[i].run();
                     if (barriers[g].getLength() >= members[i].getMaxDistance()) {
                         System.out.println("не добежал");
+                        break;
                     }
                     else System.out.println("уии добежал");
                 }
